8dfc80380003bc98dd846e0e4efe119bd7c5d4e1
Hicham-0 <mlyhichamelhanafi@gmail.com>
2026-05-30 20:43:10 +0100

    inject subnet and sg into appspec.yaml

--- terraform/modules/cicd/main.tf
+++ terraform/modules/cicd/main.tf
@@ -55,6 +55,20 @@ resource "aws_codebuild_project" "main" {
       name  = "AWS_ACCOUNT_ID"
       value = var.aws_account_id
     }
+    environment_variable {
+      name  = "SUBNET_1"
+      value = var.private_subnet_ids[0]
+    }
+
+    environment_variable {
+      name  = "SUBNET_2"
+      value = var.private_subnet_ids[1]
+    }
+
+    environment_variable {
+      name  = "SECURITY_GROUP"
+      value = var.ecs_tasks_security_group_id
+    }
   }
 
 
